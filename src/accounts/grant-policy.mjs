41c48fbe7652e5cf79375a3fe6456c2fddd319d4
Eli Sterling <eli@eliware.org>
2026-08-27 02:29:31 -0400

    Complete metadata grants for backup identities

---
+++ src/accounts/grant-policy.mjs
@@ -0,0 +1,7 @@
+const privileges = 'SELECT|INSERT|UPDATE|DELETE|EXECUTE|CREATE|ALTER|INDEX|REFERENCES|SHOW VIEW|TRIGGER|EVENT|LOCK TABLES';
+const pattern = new RegExp(`^(${privileges})(,\\s*(${privileges}))*$`, 'i');
+
+export function validateGrantPolicy(grant) {
+  if (typeof grant !== 'string' || !pattern.test(grant)) throw Object.assign(new Error('invalid grant policy'), { statusCode: 400 });
+  return grant;
+}
