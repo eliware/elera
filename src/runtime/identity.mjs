4af2d77d65604bbfb03700d90ce99ac2d53b6358
Eli Sterling <eli@eliware.org>
2026-08-29 13:30:56 -0400

    track runtime identity module

---
+++ src/runtime/identity.mjs
@@ -0,0 +1,8 @@
+import os from 'node:os';
+
+export function runtimeIdentity({ hostname = os.hostname, addresses = os.networkInterfaces } = {}) {
+  const name = hostname();
+  const interfaces = addresses();
+  const address = Object.values(interfaces).flat().find((item) => item && !item.internal && item.family === 'IPv4')?.address ?? '127.0.0.1';
+  return Object.freeze({ name, address });
+}
